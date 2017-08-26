167bc1cb23654e7f72c0f0de52a493681684d5c1
winterscall <exe.blackcherry@gmail.com>
2017-08-26 15:54:29 +0800

    embed youtube

--- resources/views/siaplapor/dashboard.blade.php
+++ resources/views/siaplapor/dashboard.blade.php
@@ -27,7 +27,27 @@
       <!-- /info alert -->
       @endif
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-6">
+          <!-- Chat box -->
+          <div class="box box-danger">
+            <div class="box-header">
+              <i class="fa fa-question-circle"></i>
+              <h3 class="box-title">Tutorial SIAPLapor</h3>
+            </div>
+            <div class="box-body">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/huvt93qLtUo" frameborder="0" allowfullscreen></iframe>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.chat -->
+          </div>
+          <!-- /.box (chat box) -->
+        </div>
+        <div class="col-md-6">
           <!-- Chat box -->
           <div class="box box-danger">
             <div class="box-header">
@@ -38,7 +58,7 @@
               <h4>Adukan dan Laporkan keluhan, kritik, dan saran anda kepada kami melalui aplikasi SIAPLapor atau pada kontak dibawah ini</h4>
               <br>
               <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-6">
                   <div class="box box-warning box-solid">
                     <div class="box-header with-border">
                       <i class="fa fa-envelope"></i>
@@ -52,7 +72,7 @@
                   </div>
                   <!-- /.box -->
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-6">
                   <div class="box box-warning box-solid">
                     <div class="box-header with-border">
                       <i class="fa fa-phone"></i>
@@ -66,7 +86,9 @@
                   </div>
                   <!-- /.box -->
                 </div>
-                <div class="col-md-3">
+              </div>
+              <div class="row">
+                <div class="col-md-6">
                   <div class="box box-warning box-solid">
                     <div class="box-header with-border">
                       <i class="fa fa-comments-o"></i>
@@ -80,7 +102,7 @@
                   </div>
                   <!-- /.box -->
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-6">
                   <div class="box box-warning box-solid">
                     <div class="box-header with-border">
                       <i class="fa fa-question-circle"></i>
